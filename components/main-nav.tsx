c52807f8e16243a70343b758c8642f5de2d624a0
thiger7 <adf0423@gmail.com>
2024-06-29 21:32:56 +0900

    first commit

---
+++ components/main-nav.tsx
@@ -0,0 +1,28 @@
+import { NavItem } from "@/types";
+import Link from "next/link";
+
+interface MainNavProps {
+  items?: NavItem[];
+  children?: React.ReactNode;
+}
+
+export default function MainNav({ items }: MainNavProps) {
+  return (
+    <div className="flex items-center md:gap-10">
+      <Link href={"/"} className="hidden md:flex items-center space-x-2">
+        <span className="font-bold hidden sm:inline-block">Post Writer</span>
+      </Link>
+      <nav className="md:flex gap-6 hidden">
+        {items?.map((item, index) => (
+          <Link
+            key={index}
+            href={item.href}
+            className="text-lg sm:text-sm font-medium hover:text-foreground/80"
+          >
+            {item.title}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
+}
